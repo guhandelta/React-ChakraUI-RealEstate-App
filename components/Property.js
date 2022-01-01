1cf952109bdeb9b223c6711795f99da14e25a89b
Kala Foods <guhan.delta@gmail.com>
2022-01-01 15:02:45 -0500

    Added Footer Text with Respective Property info in each PropertyDetail page || Added NProgress config to remove loading spinnerd on page load

--- components/Property.js
+++ components/Property.js
@@ -12,9 +12,6 @@ const property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
     return (
         <Link href={`/property/${externalID}`} passHref>
             <Flex flexWrap="wrap" width="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
-                <Box>
-                    <Image src={ coverPhoto ? coverPhoto.url : defaultImage} width={400} height={200} alt="house" />
-                </Box>
                 <Box w="full">
                     <Flex justifyContent="space-between" paddingTop="2" alignItems="center">
                         <Box padding="3" color="green.400">{ isVerified && <GoVerified />}</Box>
@@ -23,13 +20,13 @@ const property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, 
                     <Box>
                         <Avatar size="sm" src={agency?.logo?.url} />
                     </Box>
+                    <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
+                        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+                    </Flex>
+                    <Text fontSize="lg">
+                        { title.length > 30 ? `${title.substring(0, 30)}...` : title }
+                    </Text>
                 </Box>
-                <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
-                    {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
-                </Flex>
-                <Text fontSize="lg">
-                    { title.length > 30 ? `${title.substring(0, 30)}...` : title }
-                </Text>
             </Flex>
         </Link>
     )
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -9,30 +9,66 @@ import { baseUrl, fetchApi } from 'Utils/fetchApis';
 import { ImageScrollBar } from 'components';
 
 
-const PropertyDetails = ({ propertyInfo: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, amenities, photos} }) => {
+const PropertyDetails = ({ propertyInfo: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, description, type, purpose, amenities, photos, furnishingStatus } }) => {
+
+    //  amenities.map(item => item.amenities.map(amenity => console.log(amenity)));
+    //  amenities.map(item => item.amenities.map(amenity => amenity));
+
     return (
         <Box maxWidth="1080px" margin="auto" p="4">
             {photos && <ImageScrollBar data={photos} />}
-            <Box w="full" p="6">
-                <Flex flexWrap="wrap" width="420px" p="5" paddingTop="0" justifyContent="flex-start" cursor="pointer">
-                    <Box>
-                        <Image src={coverPhoto?coverPhoto.url:defaultImage} width={400} height={200} alt="house" />
-                    </Box>
-                    <Box w="full">
-                        <Flex justifyContent="space-between" paddingTop="2" alignItems="center">
-                            <Box padding="3" color="green.400">{ isVerified && <GoVerified />}</Box>
-                            <Text fontWeight="bold" fontSize="lg">AED {millify(price)}{ rentFrequency && `/${rentFrequency}`}</Text>
-                        </Flex>
-                        <Box>
-                            <Avatar size="sm" src={agency?.logo?.url} />
-                        </Box>
-                    </Box>
-                    <Flex alignItems="center" p="1" justifyContent="space-between" w="250px" color="blue.400">
-                        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+            <Box w='full' p='6'>
+                <Flex justifyContent="space-between" paddingTop="2" alignItems="center">
+                    <Flex paddingTop='2' alignItems='center'>
+                        <Box paddingRight='3' color='green.400'>{isVerified && <GoVerified />}</Box>
+                        <Text fontWeight='bold' fontSize='lg'>AED {price} {rentFrequency && `/${rentFrequency}`}</Text>
                     </Flex>
-                    <Text fontSize="lg">
-                        {title}
-                    </Text>
+                    <Box>
+                        <Avatar size='sm' src={agency?.logo?.url}></Avatar>
+                    </Box>
+                </Flex>
+                <Flex alignItems='center' p='1' justifyContent='space-between' w='250px' color='blue.400'>
+                    {rooms}<FaBed /> | {baths} <FaBath /> | {millify(area)} sqft <BsGridFill />
+                </Flex>
+                <Box marginTop="2">
+                    <Text fontSize='lg' marginBottom="2" fontWeight='bold'>{title}</Text>
+                     <Text lineHeight='2' color='gray.600'>{description}</Text>
+                </Box>
+                <Flex flexWrap="wrap" textTransform="uppercase" justifyContent="space-between">
+                    <Flex justifyContent="space-between" w="400px" borderBottom="1px" borderColor="gray.100" p="3">
+                        <Text>Type</Text>
+                        <Text fontWeight="bold">{type}</Text>
+                    </Flex>
+                    <Flex justifyContent="space-between" w="400px" borderBottom="1px" borderColor="gray.100" p="3">
+                        <Text>Property Type</Text>
+                        <Text fontWeight="bold">{purpose}</Text>
+                    </Flex>
+                    {furnishingStatus &&
+                        <Flex justifyContent="space-between" w="400px" borderBottom="1px" borderColor="gray.100" p="3">
+                            <Text>Furnishing Status</Text> 
+                            <Text fontWeight="bold">{furnishingStatus}</Text>
+                        </Flex>
+                    }
+                    <Box>
+                        {/* {amenities.length && <Text fontSize="2xl" fontWeight="bold" marginTop="5">Amenities</Text>} */}
+                        {/* <Flex flexWrap="wrap">
+                            {amenities.map(item =>(
+                                item.amenities.map(amenity => (
+                                    <Text
+                                     fontWeight="bold"
+                                     color="blue.400"
+                                     fontSize="1"
+                                     p="2"
+                                     bg="gray.200"
+                                     m="1"
+                                     borderRadius="5"
+                                     key={amenity.externalID}
+                                    >{amenity.text}</Text>
+                                ))
+                                ))
+                            }
+                        </Flex> */}
+                    </Box>
                 </Flex>
             </Box>
         </Box>
