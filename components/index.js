01cf6a00627b12a195d1bb0de78c0f6c75d9a178
Kala Foods <guhan.delta@gmail.com>
2022-01-01 07:44:29 -0500

    Created the ImageScrollBar component to scroll through the Images of the property in the respective Property Detail page

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Layout from './Layout'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import SearchFilters from './SearchFilters'
+import ImageScrollBar from './ImageScrollBar'
 
-export { Property, Layout, Navbar, Footer, SearchFilters };
+export { Property, Layout, Navbar, Footer, SearchFilters, ImageScrollBar };
